ff7be204efd241b2a2d86d4a75d796284154d188
ShaileshSSC <shaileshchotoe2001@gmail.com>
2021-03-05 10:29:24 +0100

    tests

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -1,10 +1,10 @@
 <?php
 class MyLogger
 {
-    public function log($logger) 
+    public function log($logger)
     {
         echo $logger;
     }
 }
 $logger = new MyLogger();
-$logger->log("Het werkt!");
+$logger->log("Het werktdd!");
